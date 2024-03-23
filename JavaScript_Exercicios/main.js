664612238201d6387f0668ccffc7e5d51bbce286
LuizGup <117296493+LuizGup@users.noreply.github.com>
2024-03-23 16:52:24 -0300

    changes

--- JavaScript_Exercicios/main.js
+++ JavaScript_Exercicios/main.js
@@ -1,14 +1,16 @@
-// function exibeNome() {
-//   let email = document.getElementById("email").value
-//   let nome = document.getElementById("nome").value
-//   document.getElementById("app").innerHTML = `${email} <br> ${nome}`;
-// }
+function exibeNome() {
+  let email = document.getElementById("email").value
+  let nome = document.getElementById("nome").value
+  document.getElementById("app").innerHTML = `${email} <br> ${nome}`;
+}
 
 document.getElementById("btn-nome").addEventListener("click", function () {
   let email = document.getElementById("email").value
   let nome = document.getElementById("nome").value
 
   document.getElementById("app").innerHTML = `${email} <br> ${nome}`;
+  document.getElementById("app").setAttribute("class", "eita")
+
 })
 
 document.getElementById("email").addEventListener("change", function () {
