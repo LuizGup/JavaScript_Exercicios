ee704223b2c478db8f4f42bf468cfab31a142e3b
LuizGup <117296493+LuizGup@users.noreply.github.com>
2024-03-23 14:56:14 -0300

    changes

--- Projeto1/indexP1.js
+++ Projeto1/indexP1.js
@@ -4,3 +4,13 @@ const span1 = document.getElementById("span");
 function text(){
     document.getElementById('span').textContent = 'Hello World!';
 }
+
+function setupCounter(element) {
+    let counter = 0
+    const setCounter = (count) => {
+      counter = count
+      element.innerHTML = `count is ${counter}`
+    }
+    element.addEventListener('click', () => setCounter((counter + 1)*(counter + 1)))
+    setCounter(0)
+  }
