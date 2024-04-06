5250b5fc40f1710d44d20b1077e7c47ef466bcf3
LuizGup <117296493+LuizGup@users.noreply.github.com>
2024-04-06 14:56:04 -0300

    changes

--- Projeto2/indexP2.js
+++ Projeto2/indexP2.js
@@ -1,6 +1,19 @@
-const span1 = document.getElementById("span");
 
-
-function text(){
-    document.getElementById('span').textContent = 'Hello World!';
-}
+function soma() {
+    let numeroA = document.getElementById('numeroA').value;
+    let numeroB = document.getElementById('numeroB').value;
+  
+    if (!numeroA) {
+      alert('Preencha o primeiro número');
+      return;
+    }
+  
+    if (numeroB == '') {
+      alert('Preencha o segundo número');
+      return;
+    }
+  
+    let resultado = parseInt(numeroA) + parseInt(numeroB);
+  
+    document.getElementById('resultado').innerHTML = resultado;
+  }
--- Projeto3/indexP3.js
+++ Projeto3/indexP3.js
@@ -1,6 +1,29 @@
-const span1 = document.getElementById("span");
+function soma() {
+    let numeroA = parseInt(document.getElementById('numeroA').value);
+    let numeroB = parseInt(document.getElementById('numeroB').value);
 
+    if (!numeroA) {
+        alert('Preencha o primeiro número');
+        return;
+    }
 
-function text(){
-    document.getElementById('span').textContent = 'Hello World!';
+    if (numeroB == '') {
+        alert('Preencha o segundo número');
+        return;
+    }
+    function somar(a, b) {
+        return a + b;
+    }
+    function subtrair(a, b) {
+        return a - b;
+    }
+    function multiplicar(a, b) {
+        return a * b;
+    }
+    function dividir(a, b) {
+        return a / b;
+    }
+
+    document.getElementById('resultado').innerHTML = `<br> Sua soma deu ${somar(numeroA, numeroB)}<br> Sua subtração deu ${subtrair(numeroA, numeroB)}<br> A multiplicação deu ${multiplicar(numeroA, numeroB)}<br> A divisão deu ${dividir(numeroA, numeroB)}`
+
 }
